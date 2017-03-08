736d07c634f4a68a86e17cdbe391ff82d9614c40
Ondrej Sevcik <ondrej.sev@gmail.com>
2017-03-08 15:22:28 +1100

    Add unit test support

---
+++ test/CloudBoilerplateNet.Tests/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using CloudBoilerplateNet.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+namespace CloudBoilerplateNet.Tests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void IndexTests()
+        {
+            var controller = new HomeController();
+            var result = controller.Index();
+
+            Assert.AreEqual(typeof(ViewResult), result.GetType());
+        }
+    }
+}
