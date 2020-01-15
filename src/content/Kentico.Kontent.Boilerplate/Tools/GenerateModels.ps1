6a717bbe3f3a6fb14e57a1a8f4c11464d4f6d51e
Petr Švihlík <rocky.intel@gmail.com>
2020-01-15 17:02:30 +0100

    changed path

--- src/content/Kentico.Kontent.Boilerplate/Tools/GenerateModels.ps1
+++ src/content/Kentico.Kontent.Boilerplate/Tools/GenerateModels.ps1
@@ -1,2 +1,2 @@
 ﻿dotnet tool restore
-dotnet tool run KontentModelGenerator -p "975bf280-fd91-488c-994c-2f04416e5ee3" -o ".\Models\ContentTypes" -n "Kentico.Kontent.Boilerplate.Models" -g
+dotnet tool run KontentModelGenerator -p "975bf280-fd91-488c-994c-2f04416e5ee3" -o "..\Models\ContentTypes" -n "Kentico.Kontent.Boilerplate.Models" -g
